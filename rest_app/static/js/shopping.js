5244de44a65472460b3764787ee6513ad8b30129
sstefanyuk <stefanyuk.andriyigor@gmail.com>
2022-01-15 21:46:39 +0100

    added set and modify storage

--- rest_app/static/js/shopping.js
+++ rest_app/static/js/shopping.js
@@ -22,80 +22,27 @@ $(document).ready(function () {
             }
         });
     });
-});
 
 
-function setCartToStorage(cartId) {
-    const newCart = { id: cartId, quantity: 1 };
-    localStorage.getItem('summarizeItemsDetails') ? modifyStorage(newCart) :  localStorage.setItem("summarizeItemsDetails", JSON.stringify([{ id: cartId, quantity: 1 }]));
-}
-
-function modifyStorage() {
-    let temporaryStorage = JSON.parse(localStorage.getItem('summarizeItemsDetails'));
-    temporaryStorage.push(newCart);
-    console.log(temporaryStorage, 'Temporary Storage');
-    localStorage.setItem("summarizeItemsDetails",  JSON.stringify(temporaryStorage));
-}
-
-function removeAddClassWithAnimation(element){
-    element.removeClass('hide')
-    element.addClass('show')
-
-    setTimeout(function(){
-            element.addClass('hide')
-            element.removeClass('show')
-        }, 1200)
-}
-
-
-$(document).ready(function (){
     if (document.getElementById('flashed_personal_profile_info')){
         const flashedInfo = $('#flashed_personal_profile_info')
         removeAddClassWithAnimation(flashedInfo)
     }
-});
 
 
+    $('.remove_button').on('click', function () {
+        const product_id = $(this).attr('data-product');
 
-$(document).ready(function (){
-   $('.remove_button').on('click', function (){
-       const product_id = $(this).attr('data-product');
-
-       req = $.ajax({
+        req = $.ajax({
             url: '/delete_item/' + product_id,
             type: 'POST'
         })
 
-       req.done(function(data){
-           if (data.url){
+        req.done(function (data) {
+            if (data.url) {
                 window.location = data.url
-           }
-           else {
-               $('.items_in_cart_info').html(data)
-           }
-       });
-   });
-});$(document).ready(function () {
-    $('.add_to_cart_btn').on('click', function () {
-        const product_id = $(this).attr('data-product_id');
-
-        req = $.ajax({
-            url: '/add_item_to_cart',
-            type: 'POST',
-            data: {id: product_id}
-        })
-
-        req.done(function(data) {
-            if (data.message){
-                const alertElement = $('.alert')
-                removeAddClassWithAnimation(alertElement)
-            }
-            else if (data.url){
-                window.location = data.url;
-            }
-            else {
+            } else {
                 $('.items_in_cart_info').html(data)
-                setCartToStorage(product_id);
             }
         });
     });
@@ -103,14 +50,13 @@ $(document).ready(function (){
 
 
 function setCartToStorage(cartId) {
-    const newCart = { id: cartId, quantity: 1 };
-    localStorage.getItem('summarizeItemsDetails') ? modifyStorage(newCart) :  localStorage.setItem("summarizeItemsDetails", JSON.stringify([{ id: cartId, quantity: 1 }]));
+    const newCart =  [{ id: cartId, quantity: 1 }];
+    localStorage.getItem('summarizeItemsDetails') ? modifyStorage(cartId) : localStorage.setItem("summarizeItemsDetails", JSON.stringify(newCart));
 }
 
-function modifyStorage() {
+function modifyStorage(cartId) {
     let temporaryStorage = JSON.parse(localStorage.getItem('summarizeItemsDetails'));
-    temporaryStorage.push(newCart);
-    console.log(temporaryStorage, 'Temporary Storage');
+    temporaryStorage.push({ id: cartId, quantity: 1 });
     localStorage.setItem("summarizeItemsDetails",  JSON.stringify(temporaryStorage));
 }
 
@@ -123,187 +69,3 @@ function removeAddClassWithAnimation(element){
             element.removeClass('show')
         }, 1200)
 }
-
-
-$(document).ready(function (){
-    if (document.getElementById('flashed_personal_profile_info')){
-        const flashedInfo = $('#flashed_personal_profile_info')
-        removeAddClassWithAnimation(flashedInfo)
-    }
-});
-
-
-
-$(document).ready(function (){
-   $('.remove_button').on('click', function (){
-       const product_id = $(this).attr('data-product');
-
-       req = $.ajax({
-            url: '/delete_item/' + product_id,
-            type: 'POST'
-        })
-
-       req.done(function(data){
-           if (data.url){
-                window.location = data.url
-           }
-           else {
-               $('.items_in_cart_info').html(data)
-           }
-       });
-   });
-});$(document).ready(function () {
-    $('.add_to_cart_btn').on('click', function () {
-        const product_id = $(this).attr('data-product_id');
-
-        req = $.ajax({
-            url: '/add_item_to_cart',
-            type: 'POST',
-            data: {id: product_id}
-        })
-
-        req.done(function(data) {
-            if (data.message){
-                const alertElement = $('.alert')
-                removeAddClassWithAnimation(alertElement)
-            }
-            else if (data.url){
-                window.location = data.url;
-            }
-            else {
-                $('.items_in_cart_info').html(data)
-                setCartToStorage(product_id);
-            }
-        });
-    });
-});
-
-
-function setCartToStorage(cartId) {
-    const newCart = { id: cartId, quantity: 1 };
-    localStorage.getItem('summarizeItemsDetails') ? modifyStorage(newCart) :  localStorage.setItem("summarizeItemsDetails", JSON.stringify([{ id: cartId, quantity: 1 }]));
-}
-
-function modifyStorage() {
-    let temporaryStorage = JSON.parse(localStorage.getItem('summarizeItemsDetails'));
-    temporaryStorage.push(newCart);
-    console.log(temporaryStorage, 'Temporary Storage');
-    localStorage.setItem("summarizeItemsDetails",  JSON.stringify(temporaryStorage));
-}
-
-function removeAddClassWithAnimation(element){
-    element.removeClass('hide')
-    element.addClass('show')
-
-    setTimeout(function(){
-            element.addClass('hide')
-            element.removeClass('show')
-        }, 1200)
-}
-
-
-$(document).ready(function (){
-    if (document.getElementById('flashed_personal_profile_info')){
-        const flashedInfo = $('#flashed_personal_profile_info')
-        removeAddClassWithAnimation(flashedInfo)
-    }
-});
-
-
-
-$(document).ready(function (){
-   $('.remove_button').on('click', function (){
-       const product_id = $(this).attr('data-product');
-
-       req = $.ajax({
-            url: '/delete_item/' + product_id,
-            type: 'POST'
-        })
-
-       req.done(function(data){
-           if (data.url){
-                window.location = data.url
-           }
-           else {
-               $('.items_in_cart_info').html(data)
-           }
-       });
-   });
-});$(document).ready(function () {
-    $('.add_to_cart_btn').on('click', function () {
-        const product_id = $(this).attr('data-product_id');
-
-        req = $.ajax({
-            url: '/add_item_to_cart',
-            type: 'POST',
-            data: {id: product_id}
-        })
-
-        req.done(function(data) {
-            if (data.message){
-                const alertElement = $('.alert')
-                removeAddClassWithAnimation(alertElement)
-            }
-            else if (data.url){
-                window.location = data.url;
-            }
-            else {
-                $('.items_in_cart_info').html(data)
-                setCartToStorage(product_id);
-            }
-        });
-    });
-});
-
-
-function setCartToStorage(cartId) {
-    const newCart = { id: cartId, quantity: 1 };
-    localStorage.getItem('summarizeItemsDetails') ? modifyStorage(newCart) :  localStorage.setItem("summarizeItemsDetails", JSON.stringify([{ id: cartId, quantity: 1 }]));
-}
-
-function modifyStorage() {
-    let temporaryStorage = JSON.parse(localStorage.getItem('summarizeItemsDetails'));
-    temporaryStorage.push(newCart);
-    console.log(temporaryStorage, 'Temporary Storage');
-    localStorage.setItem("summarizeItemsDetails",  JSON.stringify(temporaryStorage));
-}
-
-function removeAddClassWithAnimation(element){
-    element.removeClass('hide')
-    element.addClass('show')
-
-    setTimeout(function(){
-            element.addClass('hide')
-            element.removeClass('show')
-        }, 1200)
-}
-
-
-$(document).ready(function (){
-    if (document.getElementById('flashed_personal_profile_info')){
-        const flashedInfo = $('#flashed_personal_profile_info')
-        removeAddClassWithAnimation(flashedInfo)
-    }
-});
-
-
-
-$(document).ready(function (){
-   $('.remove_button').on('click', function (){
-       const product_id = $(this).attr('data-product');
-
-       req = $.ajax({
-            url: '/delete_item/' + product_id,
-            type: 'POST'
-        })
-
-       req.done(function(data){
-           if (data.url){
-                window.location = data.url
-           }
-           else {
-               $('.items_in_cart_info').html(data)
-           }
-       });
-   });
-});
